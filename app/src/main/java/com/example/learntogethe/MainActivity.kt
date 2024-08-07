7e864ce13aa5cd00579dedd615b49425f74cab82
riyousya <riyousya@DESKTOP-4PI3E2I>
2024-08-07 14:41:01 +0900

    SetArticleTextをテキスト表示するように変更。 Previewのための関数を追加。

--- app/src/main/java/com/example/learntogethe/MainActivity.kt
+++ app/src/main/java/com/example/learntogethe/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.learntogethe.ui.theme.LearnTogetheTheme
 
@@ -20,8 +23,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             LearnTogetheTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    SetArticleText(
+                        articleTitle = stringResource(R.string.article_title),
+                        articleText1 = stringResource(R.string.article_body1),
+                        articleText2 = stringResource(R.string.article_body2),
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +36,37 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun SetArticleText(articleTitle:String,
+                   articleText1:String,
+                   articleText2:String,
+                   modifier: Modifier = Modifier) {
+    Row(
+
     )
+    {
+        Text(
+            text = articleTitle
+        )
+        Text(
+            text = articleText1
+        )
+        Text(
+            text = articleText2
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
+    (showBackground = true,
+    showSystemUi = true)
+
 @Composable
-fun GreetingPreview() {
+fun SetArticlePreview() {
     LearnTogetheTheme {
-        Greeting("Android")
+        SetArticleText(
+            articleTitle = stringResource(R.string.article_title),
+            articleText1 = stringResource(R.string.article_body1),
+            articleText2 = stringResource(R.string.article_body2),
+        )
     }
 }
